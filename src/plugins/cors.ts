59169b668822f25b50b6ec64726ad7aa23d9aea0
Naihtton <whitediary1@outlook.com>
2023-10-10 18:22:03 +0700

    Fix CORS

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -12,8 +12,6 @@ import fp from 'fastify-plugin';
 
 export default fp(async (fastify) => {
     fastify.register(cors, {
-        // origin: ['http://localhost:5173'],
-        // credentials: true,
         origin: '*',
     });
 });
